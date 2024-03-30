fd8a07535affb76c5a27eded477402aee10e636c
Duong Ly <lyduong.tech@gmail.com>
2024-03-30 19:57:34 +0700

    temp index page

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <main className="h-full flex items-center justify-center">
+      <div><b>BoringPpl</b> By Daphne Huynh</div>
+    </main>
+  );
+}
